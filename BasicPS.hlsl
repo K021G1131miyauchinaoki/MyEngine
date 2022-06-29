993ab7e2493f4f457f9e3d640616a97a18e084fa
naoki147 <k021g1131@g.neec.ac.jp>
2022-06-29 11:05:26 +0900

    修正

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -5,7 +5,7 @@ SamplerState	smp : register(s0);
 
 float4 main(VSOutput	input) : SV_TARGET
 {
-	return float4(input.normal,1);//RGBをそれぞれ法線のXYZAを1で出力
-	//return float4(tex.Sample(smp,input.uv))*color;
+	//return float4(input.normal,1);//RGBをそれぞれ法線のXYZAを1で出力
+	return float4(tex.Sample(smp,input.uv))*color;
 	//return	float4(1,1,1,1);
 }
