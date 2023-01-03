7fc2fe73cf45777fdda5777b65585af41cd97b2a
k021g1131@g.neec.ac.jp <mk021g1131@g.neec.ac.jp>
2023-01-03 10:30:18 +0900

    コメントアウト

--- Resources/shaders/Object3dPS.hlsl
+++ Resources/shaders/Object3dPS.hlsl
@@ -1,10 +1,9 @@
-#include"Sprite.hlsli"
-
+#include"Object3d.hlsli"
 Texture2D<float4>		tex:register(t0);
 SamplerState	smp : register(s0);
 
 float4 main(VSOutput	input) : SV_TARGET
 {
-	return float4(tex.Sample(smp,input.uv))*color;
-	//return	float4(color);
+	return float4(tex.Sample(smp,input.uv)) * color;
+//return	float4(color);
 }
