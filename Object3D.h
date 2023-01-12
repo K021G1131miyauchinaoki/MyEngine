3c8113940b50e3f919e09f608a81d491a21617e2
k021g1131@g.neec.ac.jp <mk021g1131@g.neec.ac.jp>
2023-01-12 23:39:58 +0900

    gitignoreの変更によってobjファイルを追加

--- Object3D.h
+++ Object3D.h
@@ -1,88 +1,182 @@
 #pragma once
+
+#include <Windows.h>
+#include <wrl.h>
+#include <d3d12.h>
+#include <DirectXMath.h>
+#include "DirectXTex/d3dx12.h"
+#include"Model.h"
+
+/// <summary>
+/// 3Dオブジェクト
+/// </summary>
 class Object3d
 {
-};
+private: // エイリアス
+	// Microsoft::WRL::を省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+	// DirectX::を省略
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMMATRIX = DirectX::XMMATRIX;
 
-//struct Object3d
-//{
-//	//定数バッファ（行列用）
-//	ID3D12Resource* constBuffTransform;
-//	//定数バッファマップ（行列用）
-//	ConstBufferDataTransform* constMapTransform;
-//	//アフィン変換情報
-//	XMFLOAT3	scale = { 1,1,1 };
-//	XMFLOAT3	rotation = { 0,0,0 };
-//	XMFLOAT3	position = { 0,0,0 };
-//	//ワールド変換行列
-//	XMMATRIX	matWorld;
-//	//親オブジェクトへのポインタ
-//	Object3d* parent = nullptr;
-//};
-//
-//void	InitializeObject3d(Object3d* object, ID3D12Device* device)
-//{
-//	HRESULT	result;
-//
-//	//定数バッファのヒープ設定
-//	D3D12_HEAP_PROPERTIES	heapProp{};
-//	heapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
-//	//定数バッファのリソース設定
-//	D3D12_RESOURCE_DESC	resdesc{};
-//	resdesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-//	resdesc.Width = (sizeof(ConstBufferDataTransform) + 0xff) & ~0xff;
-//	resdesc.Height = 1;
-//	resdesc.DepthOrArraySize = 1;
-//	resdesc.MipLevels = 1;
-//	resdesc.SampleDesc.Count = 1;
-//	resdesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-//
-//	//定数バッファの生成
-//	result = device->CreateCommittedResource(
-//		&heapProp,//ヒープ設定
-//		D3D12_HEAP_FLAG_NONE,
-//		&resdesc,//リソース設定
-//		D3D12_RESOURCE_STATE_GENERIC_READ,
-//		nullptr,
-//		IID_PPV_ARGS(&object->constBuffTransform));
-//	assert(SUCCEEDED(result));
-//	//定数バッファのマッピング
-//	result = object->constBuffTransform->Map(0, nullptr, (void**)&object->constMapTransform);//マッピング
-//	assert(SUCCEEDED(result));
-//}
-//
-//void	UpdateObject3d(Object3d* object, XMMATRIX& matview, XMMATRIX& matProjection) {
-//	XMMATRIX	matScale, matRot, matTrans;
-//	//スケール、回転、平行移動の行列計算
-//	matScale = XMMatrixScaling(object->scale.x, object->scale.y, object->scale.z);
-//	matRot = XMMatrixIdentity();
-//	matRot *= XMMatrixRotationZ(object->rotation.z);
-//	matRot *= XMMatrixRotationX(object->rotation.x);
-//	matRot *= XMMatrixRotationY(object->rotation.y);
-//	matTrans = XMMatrixTranslation(object->position.x, object->position.y, object->position.z);
-//
-//	//行列の合成
-//	object->matWorld = XMMatrixIdentity();
-//	object->matWorld *= matScale;
-//	object->matWorld *= matRot;
-//	object->matWorld *= matTrans;
-//
-//	//親オブジェクトがあれば
-//	if (object->parent != nullptr)
-//	{
-//		object->matWorld *= object->parent->matWorld;
-//	}
-//	//定数バッファへデータ転送
-//	object->constMapTransform->mat = object->matWorld * matview * matProjection;
-//}
-//
-//void	DrawObject3d(Object3d* object, ID3D12GraphicsCommandList* commandList, D3D12_VERTEX_BUFFER_VIEW& vbView,
-//	D3D12_INDEX_BUFFER_VIEW& ibView, UINT	numIndices) {
-//	//頂点バッファの設定
-//	commandList->IASetVertexBuffers(0, 1, &vbView);
-//	//インデックスバッファの設定
-//	commandList->IASetIndexBuffer(&ibView);
-//	//定数バッファビュー(CBV)の設定コマンド
-//	commandList->SetGraphicsRootConstantBufferView(2, object->constBuffTransform->GetGPUVirtualAddress());
-//	//描画コマンド
-//	commandList->DrawIndexedInstanced(numIndices, 1, 0, 0, 0);
-//}
+public: // サブクラス
+	// 定数バッファ用データ構造体
+	struct ConstBufferDataB0
+	{
+		//XMFLOAT4 color;	// 色 (RGBA)
+		XMMATRIX mat;	// ３Ｄ変換行列
+	};
+
+private: // 定数
+	static const int division = 50;					// 分割数
+	static const float radius;				// 底面の半径
+	static const float prizmHeight;			// 柱の高さ
+	static const int planeCount = division * 2 + division * 2;		// 面の数
+	static const int vertexCount = planeCount * 3;		// 頂点数
+
+public: // 静的メンバ関数
+	/// <summary>
+	/// 静的初期化
+	/// </summary>
+	/// <param name="device">デバイス</param>
+	/// <param name="window_width">画面幅</param>
+	/// <param name="window_height">画面高さ</param>
+	static void StaticInitialize(ID3D12Device* device, int window_width, int window_height);
+
+	/// <summary>
+	/// 描画前処理
+	/// </summary>
+	/// <param name="cmdList">描画コマンドリスト</param>
+	static void PreDraw(ID3D12GraphicsCommandList* cmdList);
+
+	/// <summary>
+	/// 描画後処理
+	/// </summary>
+	static void PostDraw();
+
+	/// <summary>
+	/// 3Dオブジェクト生成
+	/// </summary>
+	/// <returns></returns>
+	static Object3d* Create();
+
+	/// <summary>
+	/// 視点座標の取得
+	/// </summary>
+	/// <returns>座標</returns>
+	static const XMFLOAT3& GetEye() { return eye; }
+
+	/// <summary>
+	/// 視点座標の設定
+	/// </summary>
+	/// <param name="position">座標</param>
+	static void SetEye(XMFLOAT3 eye);
+
+	/// <summary>
+	/// 注視点座標の取得
+	/// </summary>
+	/// <returns>座標</returns>
+	static const XMFLOAT3& GetTarget() { return target; }
+
+	/// <summary>
+	/// 注視点座標の設定
+	/// </summary>
+	/// <param name="position">座標</param>
+	static void SetTarget(XMFLOAT3 target);
+
+	/// <summary>
+	/// ベクトルによる移動
+	/// </summary>
+	/// <param name="move">移動量</param>
+	static void CameraMoveVector(XMFLOAT3 move);
+
+private: // 静的メンバ変数
+	// デバイス
+	static ID3D12Device* device;
+	// コマンドリスト
+	static ID3D12GraphicsCommandList* cmdList;
+	// ルートシグネチャ
+	static ComPtr<ID3D12RootSignature> rootsignature;
+	// パイプラインステートオブジェクト
+	static ComPtr<ID3D12PipelineState> pipelinestate;
+	// ビュー行列
+	static XMMATRIX matView;
+	// 射影行列
+	static XMMATRIX matProjection;
+	// 視点座標
+	static XMFLOAT3 eye;
+	// 注視点座標
+	static XMFLOAT3 target;
+	// 上方向ベクトル
+	static XMFLOAT3 up;
+
+private:// 静的メンバ関数
+	/// <summary>
+	/// カメラ初期化
+	/// </summary>
+	/// <param name="window_width">画面横幅</param>
+	/// <param name="window_height">画面縦幅</param>
+	static void InitializeCamera(int window_width, int window_height);
+
+	/// <summary>
+	/// グラフィックパイプライン生成
+	/// </summary>
+	/// <returns>成否</returns>
+	static void InitializeGraphicsPipeline();
+
+	/// <summary>
+	/// ビュー行列を更新
+	/// </summary>
+	static void UpdateViewMatrix();
+
+public: // メンバ関数
+	bool Initialize();
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+	/// <summary>
+	/// 座標の取得
+	/// </summary>
+	/// <returns>座標</returns>
+	const XMFLOAT3& GetPosition() const { return position; }
+
+	/// <summary>
+	/// 座標の設定
+	/// </summary>
+	/// <param name="position">座標</param>
+	void SetPosition(const XMFLOAT3& position) { this->position = position; }
+
+	//セッター
+	void	SetModel(Model* model_) { model = model_; }
+
+
+
+
+private: // メンバ変数
+	//ComPtr<ID3D12Resource> constBuff; // 定数バッファ
+	ComPtr<ID3D12Resource> constBuffB0; // 定数バッファ
+	//ComPtr<ID3D12Resource> constBuffB1; // 定数バッファ
+	// 色
+	XMFLOAT4 color = { 1,1,1,1 };
+	// ローカルスケール
+	XMFLOAT3 scale = { 1,1,1 };
+	// X,Y,Z軸回りのローカル回転角
+	XMFLOAT3 rotation = { 0,0,0 };
+	// ローカル座標
+	XMFLOAT3 position = { 0,0,0 };
+	// ローカルワールド変換行列
+	XMMATRIX matWorld;
+	// 親オブジェクト
+	Object3d* parent = nullptr;
+	//モデル
+	Model* model = nullptr;
+};
