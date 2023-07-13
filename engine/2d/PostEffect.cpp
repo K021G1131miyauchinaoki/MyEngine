8f02051220623c54f2058a00a2fc3e5131703003
naoki147 <k021g1131@g.neec.ac.jp>
2023-07-13 15:34:23 +0900

    03_03 レンダーテクスチャ生成

--- engine/2d/PostEffect.cpp
+++ engine/2d/PostEffect.cpp
@@ -124,7 +124,12 @@ void PostEffect::Draw(ID3D12GraphicsCommandList* cmdList_) {
 
 	spriteCommon->SetTextureCommands(texIndex);
 	ID3D12DescriptorHeap* ppHeaps[] = { descHeapSRV.Get() };
-
+	cmdList_->SetDescriptorHeaps(_countof(ppHeaps), ppHeaps);
+	// パイプラインステートとルートシグネチャの設定コマンド
+	//cmdList_->SetPipelineState(spriteCommon->GetPiprlineState());
+	//cmdList_->SetGraphicsRootSignature(spriteCommon->GetRootSignature());
+	// プリミティブ形状の設定コマンド
+	cmdList_->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST); // 三角形リスト
 	// 頂点バッファビューの設定コマンド
 	cmdList_->IASetVertexBuffers(0, 1, &vbView);
 	//定数バッファビュー（CBV）の設定コマンド
@@ -132,7 +137,8 @@ void PostEffect::Draw(ID3D12GraphicsCommandList* cmdList_) {
 	//インディックスバッファビューの設定コマンド
 	cmdList_->IASetIndexBuffer(&ibView);
 	//定数バッファビュー(SRV)の設定コマンド
-	//cmdList_->SetGraphicsRootDescriptorTable(1, descHeapSRV->GetGPUDescriptorHandleForHeapStart());
+	D3D12_GPU_DESCRIPTOR_HANDLE descHeapSRVHandle = descHeapSRV->GetGPUDescriptorHandleForHeapStart();
+	cmdList_->SetGraphicsRootDescriptorTable(1,descHeapSRVHandle);
 	// 描画コマンド
 	cmdList_->DrawIndexedInstanced(_countof(indice), texIndex, 0, 0, 0);//全ての頂点を使って描画
 }
